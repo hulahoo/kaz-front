b47eb9a992bf5891e43a071b1a3c501226781d58
maxim.sherstoboyev@uco.kz <maxim.sherstoboyev@uco.kz>
2021-02-10 11:50:25 +0600

    portal: enable notifications

--- modules/front/src/app/header/UserPanel/Notification/NotificationDropdown/NotificationDropdown.tsx
+++ modules/front/src/app/header/UserPanel/Notification/NotificationDropdown/NotificationDropdown.tsx
@@ -5,10 +5,32 @@ import {Tabs} from "antd";
 import {RootStoreProp} from "../../../../store";
 import {DEFAULT_DATE_TIME_PATTERN_WITHOUT_SECONDS, format} from "../../../../util/Date/Date";
 import Button, {ButtonType} from "../../../../components/Button/Button";
+import {action, observable} from "mobx";
+import {BellNotification} from "../../../../store/BellNotificationStore";
+import {restServices} from "../../../../../cuba/services";
+
+const taskCode = "KPI_APPROVE";
+const notificationCode = "NOTIFICATION";
+
+const scheduleNotificationTime: number = 10000;
 
 @inject("rootStore")
 @observer
 class NotificationDropdownMenu extends Component<WrappedComponentProps & RootStoreProp> {
+
+  @observable bellNotifications: BellNotification[];
+  @observable bellTasks: BellNotification[];
+
+  @action
+  setBellNotifications = (value: BellNotification[]) => {
+    this.bellNotifications = value;
+  };
+
+  @action
+  setBellTasks = (value: BellNotification[]) => {
+    this.bellTasks = value;
+  };
+
   render() {
     const {TabPane} = Tabs;
 
@@ -18,7 +40,7 @@ class NotificationDropdownMenu extends Component<WrappedComponentProps & RootSto
           <TabPane tab="Задачи" key="1">
             <div>
               <ul className={"notifications-tab-content"}>
-                {this.props.rootStore!.bellNotification.bellTasks ? this.props.rootStore!.bellNotification.bellTasks.map(task => {
+                {this.bellTasks ? this.bellTasks.map(task => {
                   return <li key={task.id}>
                     <div className={"bell-notification-name"}>{task.name}</div>
                     <div
@@ -35,7 +57,7 @@ class NotificationDropdownMenu extends Component<WrappedComponentProps & RootSto
           <TabPane tab="Уведомления" key="2">
             <div>
               <ul className={"notifications-tab-content"}>
-                {this.props.rootStore!.bellNotification.bellNotifications ? this.props.rootStore!.bellNotification.bellNotifications.map(notification => {
+                {this.bellNotifications ? this.bellNotifications.map(notification => {
                   return <li key={notification.id}>
                     <div className={"bell-notification-name"}>{notification.name}</div>
                     <div
@@ -51,6 +73,29 @@ class NotificationDropdownMenu extends Component<WrappedComponentProps & RootSto
         </Tabs>
       </div>
     );
+  }
+
+  componentDidMount(): void {
+    const loadNotifications = () => {
+      restServices.notificationsService.notifications().then((r: string) => {
+        const notifications: BellNotification[] = [];
+        const tasks: BellNotification[] = [];
+
+        (JSON.parse(r) as BellNotification[]).forEach(e => {
+          e.createTs = new Date(e.createTs);
+          if (e.code === notificationCode) {
+            notifications.push(e);
+          } else {
+            tasks.push(e);
+          }
+        });
+
+        this.setBellNotifications(notifications);
+        this.setBellTasks(tasks);
+      });
+    };
+    loadNotifications();
+    setInterval(loadNotifications, scheduleNotificationTime);
   }
 }
 
