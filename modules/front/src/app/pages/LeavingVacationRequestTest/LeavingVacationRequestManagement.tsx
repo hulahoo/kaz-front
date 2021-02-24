826b15d487977884cb9724a6b0443c19cbe9a3ad
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-02-24 16:03:43 +0600

    adding AbsenceList

--- modules/front/src/app/pages/LeavingVacationRequestTest/LeavingVacationRequestManagement.tsx
+++ modules/front/src/app/pages/LeavingVacationRequestTest/LeavingVacationRequestManagement.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 import {RouteComponentProps} from "react-router";
 import {observer} from "mobx-react";
-import AbsenceRequestEdit from "./AbsenceRequestEdit";
+import {LeavingVacationRequestEdit} from "./LeavingVacationRequestEdit";
 
 type Props = RouteComponentProps<{ entityId: string }>;
 
 @observer
-export class AbsenceRequestManagement extends React.Component<Props> {
-  static PATH = "/absenceRequest";
+export class LeavingVacationRequestManagement extends React.Component<Props> {
+  static PATH = "/leavingVacationRequest";
   static NEW_SUBPATH = "new";
 
   render() {
     const {entityId} = this.props.match.params;
     return (
       <>
-        <AbsenceRequestEdit entityId={entityId}/>
+        <LeavingVacationRequestEdit entityId={entityId}/>
       </>
     );
   }
