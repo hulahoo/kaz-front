eab36bfbcf0c47d0bde56553aac67ec727004380
maxim.sherstoboyev@uco.kz <maxim.sherstoboyev@uco.kz>
2021-03-16 00:36:19 +0600

    change menu store

--- modules/front/src/app/store/MenuStore.ts
+++ modules/front/src/app/store/MenuStore.ts
@@ -29,187 +29,39 @@ export default class MenuStore {
   @action
   loadUserMenuList = () => {
     this.menuList = [
-      {
-        id: "main",
-        caption: "Главная",
-        items: [
-          {
-            id: "welcome",
-            caption: "Добро пожаловать",
-            menuLink: "/welcome",
-            pathPattern: "/welcome",
-            component: null
-          }, {
-            id: "information-desk",
-            caption: "Информационная доска",
-            menuLink: "/information-desk",
-            pathPattern: "/information-desk",
-            component: null
-          }, {
-            id: "birthday",
-            caption: "Сегодня день рождения",
-            menuLink: "/birthday",
-            pathPattern: "/birthday",
-            component: null
-          }, {
-            id: "new-employee",
-            caption: "Новые работники месяца",
-            menuLink: "/new-employee",
-            pathPattern: "/new-employee",
-            component: null
-          }
-        ],
-      } as MenuSubMenu,
+      {id: "main", caption: "Главная", menuLink: "/", pathPattern: "/", component: null},
       {
         id: "my-profile",
         caption: "Мой профиль",
-        items: [
-          {
-            id: "profile",
-            caption: "Добро пожаловать",
-            menuLink: "/my-profile",
-            pathPattern: "/my-profile"
-          },
-          {
-            id: "my-hr-request",
-            caption: "Мои HR заявки",
-            items: [
-              {
-                id: "request-update-personal-records",
-                caption: "Заявка на изменение личных данных",
-                menuLink: "/request-update-personal-records",
-                pathPattern: "/request-update-personal-records"
-              }, {
-                id: "vacation-schedule",
-                caption: "График отпуска",
-                menuLink: "/vacation-schedule",
-                pathPattern: "/vacation-schedule"
-              }, {
-                id: "leave-request",
-                caption: "Заявка на отпуск и отсутствия",
-                menuLink: "/leave-request",
-                pathPattern: "/leave-request"
-              }, {
-                id: "job-confirmation-letter",
-                caption: "Справка с места работы",
-                menuLink: "/job-confirmation-letter",
-                pathPattern: "/job-confirmation-letter"
-              }, {
-                id: "request-end-unpaid-child-care-leave",
-                caption: "Заявка на выход из отпуска по уходу за ребенком",
-                menuLink: "/request-end-unpaid-child-care-leave",
-                pathPattern: "/request-end-unpaid-child-care-leave"
-              }, {
-                id: "medical-insurance",
-                caption: "Добровольное медицинское страхование",
-                menuLink: "/medical-insurance",
-                pathPattern: "/medical-insurance"
-              }
-            ],
-          } as MenuSubMenu
-        ],
-      } as MenuSubMenu,
-      {
+        menuLink: "/my-profile",
+        pathPattern: "/my-profile",
+        component: null
+      }, {
         id: "my-team",
         caption: "Моя команда",
-        items: [
-          {
-            id: "employee-profile",
-            caption: "Профиль сотрудника",
-            items: [
-              {
-                id: "personal-information",
-                caption: "Добровольное медицинское страхование",
-                menuLink: "/personal-information",
-                pathPattern: "/medical-insurance"
-              }, {
-                id: "work-experience-qualification",
-                caption: "Добровольное медицинское страхование",
-                menuLink: "/work-experience-qualification",
-                pathPattern: "/work-experience-qualification"
-              }, {
-                id: "assignments",
-                caption: "Добровольное медицинское страхование",
-                menuLink: "/assignments",
-                pathPattern: "/assignments"
-              }, {
-                id: "working-time-management",
-                caption: "Добровольное медицинское страхование",
-                menuLink: "/working-time-management",
-                pathPattern: "/working-time-management"
-              }, {
-                id: "c-b",
-                caption: "Добровольное медицинское страхование",
-                menuLink: "/c-b",
-                pathPattern: "/c-b"
-              }
-            ]
-          } as MenuSubMenu,
-        ]
-      } as MenuSubMenu,
+        menuLink: "/my-team",
+        pathPattern: "/my-team",
+        component: null
+      }, {
+        id: "absence",
+        caption: "Absence",
+        menuLink: "/absence",
+        pathPattern: "/absence",
+        component: AbsenceList
+      },
       {
-        id: "hr-request-manager",
-        caption: "Главная",
+        id: "help",
+        caption: "Справка",
         items: [
           {
-            id: "org-structure-request",
-            caption: "Заявка по Орг.структуре",
-            menuLink: "/org-structure-request",
-            pathPattern: "/org-structure-request",
-            component: null,
-          }, {
-            id: "vacation-recall-request",
-            caption: "Информационная доска",
-            menuLink: "/vacation-recall-request",
-            pathPattern: "/vacation-recall-request",
-            component: null
-          }, {
-            id: "vacation-change-request",
-            caption: "Сегодня день рождения",
-            menuLink: "/vacation-change-request",
-            pathPattern: "/vacation-change-request",
-            component: null
-          }, {
-            id: "working-schedule",
-            caption: "Новые работники месяца",
-            menuLink: "/working-schedule",
-            pathPattern: "/working-schedule",
-            component: null
-          }, {
-            id: "temporary-transfer-day",
-            caption: "Новые работники месяца",
-            menuLink: "/temporary-transfer-day",
-            pathPattern: "/temporary-transfer-day",
-            component: null
-          }, {
-            id: "vacation-schedule",
-            caption: "Новые работники месяца",
-            menuLink: "/vacation-schedule",
-            pathPattern: "/vacation-schedule",
-            component: null
+            id: "certificateRequest",
+            caption: "Справка с места работы",
+            menuLink: "/certificateRequest",
+            pathPattern: "/certificateRequest/:entityId?",
+            component: CertificateRequestManagement
           }
         ],
       } as MenuSubMenu,
-      // {
-      //   id: "absence",
-      //   caption: "Absence",
-      //   menuLink: "/absence",
-      //   pathPattern: "/absence",
-      //   component: AbsenceList
-      // },
-      // {
-      //   id: "help",
-      //   caption: "Справка",
-      //   items: [
-      //     {
-      //       id: "certificateRequest",
-      //       caption: "Справка с места работы",
-      //       menuLink: "/certificateRequest",
-      //       pathPattern: "/certificateRequest/:entityId?",
-      //       component: CertificateRequestManagement
-      //     }
-      //   ],
-      // } as MenuSubMenu,
       // {
       //   id: "vacation",
       //   caption: "Отпуска",
@@ -279,6 +131,13 @@ export default class MenuStore {
       //   caption: "Job Offers",
       //   menuLink: "/job-offers",
       //   pathPattern: "/job-offers",
+      //   component: null,
+      // },
+      // {
+      //   id: "org-structure-request",
+      //   caption: "Заявка по Орг.структуре",
+      //   menuLink: "/org-structure-request",
+      //   pathPattern: "/org-structure-request",
       //   component: null,
       // }
       ]
