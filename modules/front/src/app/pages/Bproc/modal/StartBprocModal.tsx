6efdd92961538af8ef0d2d27398801b2e8324a97
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-04-13 16:58:41 +0600

    fix bug

--- modules/front/src/app/pages/Bproc/modal/StartBprocModal.tsx
+++ modules/front/src/app/pages/Bproc/modal/StartBprocModal.tsx
@@ -73,7 +73,7 @@ class StartBprocModal extends React.Component<StartBproc & MainStoreInjected & R
       .then(value => {
         this.bprocRolesDefiner = value;
         restServices.startBprocService.getNotPersisitBprocActors({
-          employee: this.props.employee ? this.props.employee() : null,
+          employee: this.props.employee ? this.props.employee() || null : null,
           initiatorPersonGroupId: this.props.rootStore!.userInfo.personGroupId!,
           bpmRolesDefiner: value
         }).then(notPersisitBprocActors => {
