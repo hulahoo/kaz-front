adb1414ceab866c6d82807559d6ec2ff1c86bc33
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-05-05 13:55:27 +0600

    KAZMIN-117

--- modules/front/src/app/pages/MyTeam/timeManagement/ChangeAbsenceDaysRequest/ChangeAbsenceDaysRequestEdit.tsx
+++ modules/front/src/app/pages/MyTeam/timeManagement/ChangeAbsenceDaysRequest/ChangeAbsenceDaysRequestEdit.tsx
@@ -36,6 +36,7 @@ import {DicPurposeAbsence} from "../../../../../cuba/entities/base/tsadv_DicPurp
 import {FileDescriptor} from "../../../../../cuba/entities/base/sys$FileDescriptor";
 import {restServices} from "../../../../../cuba/services";
 import Notification from "../../../../util/Notification/Notification";
+import TextArea from "antd/es/input/TextArea";
 
 type EditorProps = {
   entityId: string;
@@ -56,6 +57,9 @@ class ChangeAbsenceDaysRequestEdit extends AbstractBprocEdit<ChangeAbsenceDaysRe
   });
 
   @observable
+  isPurposeTypeOther = false;
+
+  @observable
   dicPurposeAbsence: DictionaryDataCollectionStore<DicPurposeAbsence>;
 
   filesDc = collection<FileDescriptor>(FileDescriptor.NAME, {
@@ -74,6 +78,8 @@ class ChangeAbsenceDaysRequestEdit extends AbstractBprocEdit<ChangeAbsenceDaysRe
     "scheduleEndDate",
 
     "purpose",
+
+    "purposeText",
 
     "newStartDate",
 
@@ -284,10 +290,42 @@ class ChangeAbsenceDaysRequestEdit extends AbstractBprocEdit<ChangeAbsenceDaysRe
                     rules: [{
                       required: true,
                       message: this.props.intl.formatMessage({id: "form.validation.required"}, {fieldName: messages[this.dataInstance.entityName + '.purpose']}),
-                    }]
+                    }],
+                    getValueFromEvent: args => {
+
+                      if (args) {
+                        const purpose = this.dicPurposeAbsence.items.find(value => value.id == args);
+                        if (purpose)
+                          this.isPurposeTypeOther = purpose.code === 'OTHER';
+                      }
+
+                      return args;
+                    }
                   }}
                   formItemOpts={{style: {marginBottom: "12px"}}}
                 />
+
+
+                <div className={"ant-row ant-form-item"}
+                     style={this.isPurposeTypeOther ? {marginBottom: "12px"} : {display: 'none'}}>
+                  {createElement(Msg, {entityName: this.dataInstance.entityName, propertyName: "purposeText"})}
+                  <Form.Item>
+                    {this.props.form.getFieldDecorator("purposeText", {
+                      rules: [{
+                        required: true,
+                        validator: (rule, value, callback) => {
+                          if (this.isPurposeTypeOther && !value)
+                            callback(this.props.intl.formatMessage({id: "form.validation.required"}, {fieldName: messages[this.dataInstance.entityName + '.purposeText']}));
+                          else callback();
+                        },
+                      }]
+                    })(
+                      <TextArea
+                        disabled={isNotDraft}
+                        rows={4}/>
+                    )}
+                  </Form.Item>
+                </div>
 
                 <ReadonlyField
                   entityName={this.dataInstance.entityName}
@@ -452,6 +490,8 @@ class ChangeAbsenceDaysRequestEdit extends AbstractBprocEdit<ChangeAbsenceDaysRe
               this.loadPerson(absence.personGroup!.id!).then(value => this.person = value);
               this.setEmployee(absence.personGroup!.id!);
             });
+
+        this.isPurposeTypeOther = (item && item.purpose && item.purpose.code === 'OTHER') == true;
 
         const obj = {
           ...this.dataInstance.getFieldValues(this.fields)
