5f9276683cb238fd9cebfe07db82776c1a8cf560
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-05-19 19:58:50 +0600

    KAZMIN-517

---
+++ modules/front/src/app/pages/LearningHistory/LearningHistoryScormResult/index.tsx
@@ -0,0 +1,78 @@
+import {getCubaREST, injectMainStore, MainStoreInjected, Msg} from "@cuba-platform/react";
+import React, {createElement} from "react";
+import {injectIntl, WrappedComponentProps} from "react-intl";
+import {observer} from "mobx-react";
+import {Card, Form, Tabs} from "antd";
+import TextArea from "antd/es/input/TextArea";
+import {CourseSectionScormResult} from "../../../../cuba/entities/base/tsadv_CourseSectionScormResult";
+
+const {TabPane} = Tabs;
+
+export type LearningHistoryScormResultProps = {
+  enrollmentId: string,
+}
+
+@injectMainStore
+@observer
+class LearningHistoryScormResult extends React.Component<LearningHistoryScormResultProps & MainStoreInjected & WrappedComponentProps> {
+
+  scormResult: CourseSectionScormResult[] = [];
+
+  render() {
+
+    return (
+      <div>
+        <Card className="narrow-layout card-actions-container">
+          <Tabs tabPosition='top'>
+            {
+              this.scormResult.map((scormResult, index) => {
+                return <TabPane
+                  tab={this.props.intl.messages[CourseSectionScormResult.NAME + '.question'] + " " + (1 + index)}
+                  key={"" + index}>
+                  <div>
+                    <Card className="narrow-layout">
+                      <Form layout="vertical">
+
+                        <div className={"ant-row ant-form-item"} style={{marginBottom: "12px"}}>
+                          {createElement(Msg, {entityName: CourseSectionScormResult.NAME, propertyName: "question"})}
+                          <TextArea
+                            value={scormResult.question ? scormResult.question.question || '' : ''}
+                            disabled={true}
+                            rows={4}/>
+                        </div>
+
+                        <div className={"ant-row ant-form-item"} style={{marginBottom: "12px"}}>
+                          {createElement(Msg, {entityName: CourseSectionScormResult.NAME, propertyName: "answer"})}
+                          <TextArea
+                            disabled={true}
+                            value={scormResult!.answer!}
+                            rows={4}/>
+                        </div>
+                      </Form>
+                    </Card>
+                  </div>
+                </TabPane>
+              })
+            }
+          </Tabs>
+        </Card>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    getCubaREST()!.searchEntities<CourseSectionScormResult>(CourseSectionScormResult.NAME, {
+      conditions: [{
+        property: 'courseSectionAttempt.enrollment.id',
+        operator: '=',
+        value: this.props.enrollmentId
+      }]
+    }, {
+      view: 'courseSectionScormResult-view'
+    }).then(scormResult => {
+      this.scormResult = scormResult;
+    });
+  }
+}
+
+export default injectIntl(LearningHistoryScormResult);
