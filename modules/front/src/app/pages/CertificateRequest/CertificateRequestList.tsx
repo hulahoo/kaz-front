b6793bccf027dd9d3f833df39be3602fd8bd518d
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-08-18 14:40:08 +0600

    adding localization in caertificate request

--- modules/front/src/app/pages/CertificateRequest/CertificateRequestList.tsx
+++ modules/front/src/app/pages/CertificateRequest/CertificateRequestList.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {inject, observer} from "mobx-react";
 import {Link} from "react-router-dom";
 
-import {collection, injectMainStore, MainStoreInjected} from "@cuba-platform/react";
+import {collection, getCubaREST, injectMainStore, MainStoreInjected} from "@cuba-platform/react";
 
 import {CertificateRequest} from "../../../cuba/entities/base/tsadv_CertificateRequest";
 import {CertificateRequestManagement} from "./CertificateRequestManagement";
@@ -14,7 +14,9 @@ import Page from "../../hoc/PageContentHoc";
 import Section from "../../hoc/Section";
 import DataTableFormat from "../../components/DataTable/intex";
 import {FileDescriptor} from "../../../cuba/entities/base/sys$FileDescriptor";
-import {downloadFile} from "../../util/util";
+import {downloadFile, isEquals} from "../../util/util";
+import {observable} from "mobx";
+import {CertificateTemplate} from "../../../cuba/entities/base/tsadv_CertificateTemplate";
 
 @injectMainStore
 @inject("rootStore")
@@ -38,6 +40,9 @@ class CertificateRequestListComponent extends React.Component<MainStoreInjected 
 
     "file"
   ];
+
+  @observable
+  templates: CertificateTemplate[] = [];
 
   render() {
     if (!this.dataCollection.items)
@@ -76,19 +81,30 @@ class CertificateRequestListComponent extends React.Component<MainStoreInjected 
                                >{text}</Link>
                              }, {
                                column: this.fields[3],
-                               render: (text, record) => {
-                                 const file = (record as CertificateRequest).file;
-                                 if (file)
+                               render: (text, record: CertificateRequest) => {
+                                 const file = record.file;
+                                 if (file) {
+                                   const template = this.templates.find(value => isEquals(value.certificateType, record.certificateType)
+                                     && isEquals(value.language, record.language)
+                                     && isEquals(value.organization && value.organization.company && value.organization.company.code, this.props.rootStore!.userInfo!.companyCode)
+                                     && isEquals(value.showSalary, record.showSalary)
+                                     && isEquals(value.receivingType, record.receivingType));
+
+                                   let reportName = template && template.report && template.report['_instanceName'];
+
+                                   if (!reportName) reportName = file.name!;
+
                                    return (
                                      <a onClick={() => {
-                                       downloadFile((file as FileDescriptor).id,
-                                         (file as FileDescriptor).name as string,
-                                         (file as FileDescriptor).extension as string,
+                                       downloadFile(file.id,
+                                         reportName,
+                                         file.extension as string,
                                          "");
                                      }}>
-                                       {(file as FileDescriptor).name}
+                                       {reportName}
                                      </a>
                                    )
+                                 }
                                  return (<span/>);
                                }
                              }]}
@@ -97,6 +113,13 @@ class CertificateRequestListComponent extends React.Component<MainStoreInjected 
         </Section>
       </Page>
     );
+  }
+
+  componentDidMount() {
+    getCubaREST()!.loadEntities<CertificateTemplate>(CertificateTemplate.NAME, {
+      view: 'portal-certificateTemplate'
+    })
+      .then(value => this.templates = value);
   }
 }
 
