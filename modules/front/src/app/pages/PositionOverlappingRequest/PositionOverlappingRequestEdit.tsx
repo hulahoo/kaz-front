f93ebfabd28ec0f89d9445112ec5655f908d1767
Dulat <dulat.issenzhulov@uco.kz>
2021-10-12 19:54:12 +0600

    KAZMIN-692

--- modules/front/src/app/pages/PositionOverlappingRequest/PositionOverlappingRequestEdit.tsx
+++ modules/front/src/app/pages/PositionOverlappingRequest/PositionOverlappingRequestEdit.tsx
@@ -34,6 +34,7 @@ type EditorProps = {
   entityId: string,
   personGroupId: any;
 };
+
 @inject("rootStore")
 @injectMainStore
 @observer
@@ -42,7 +43,7 @@ class PositionOverlappingRequestEditComponent extends AbstractBprocEdit<Position
 
   dataInstance = instance<PositionOverlappingRequest>(
     PositionOverlappingRequest.NAME,
-    { view: "positionOverlappingRequest-edit", loadImmediately: false }
+    {view: "positionOverlappingRequest-edit", loadImmediately: false}
   );
 
   statusesDc = collection<DicRequestStatus>(DicRequestStatus.NAME, {
@@ -54,9 +55,23 @@ class PositionOverlappingRequestEditComponent extends AbstractBprocEdit<Position
     {view: "_minimal"}
   )
 
+  @observable
+  currentOrganization: string | undefined | null
+
   positionGroupDc = collection<PositionGroupExt>(
     PositionGroupExt.NAME,
-    {view: "_minimal"}
+    {
+      filter: {
+        conditions: [
+          {
+            property: "organizationGroupExt",
+            operator: "=",
+            value: this.currentOrganization ? this.currentOrganization : null
+          }
+        ]
+      },
+      view: "_minimal"
+    }
   )
 
   organizationGroupDc = collection<OrganizationGroup>(
@@ -66,8 +81,6 @@ class PositionOverlappingRequestEditComponent extends AbstractBprocEdit<Position
 
   @observable
   editPositionOverlappingRequest: PositionOverlappingRequest;
-
-
 
 
   fields = [
@@ -138,169 +151,174 @@ class PositionOverlappingRequestEditComponent extends AbstractBprocEdit<Position
 
 
     return <Page pageName={<MsgEntity entityName={PositionOverlappingRequest.NAME}/>}>
-              <Section size="large">
-                  <Card className="narrow-layout card-actions-container"
-                        bordered={false}
-                        actions={[
-                          <Button buttonType={ButtonType.FOLLOW}
-                                  onClick={() => goBackOrHomePage(this.props.history!)}>{this.props.intl.formatMessage({id: "close"})}</Button>,
-                          this.getOutcomeBtns()]}
-                  >
-                    <Form onSubmit={this.validate} layout="vertical">
+      <Section size="large">
+        <Card className="narrow-layout card-actions-container"
+              bordered={false}
+              actions={[
+                <Button buttonType={ButtonType.FOLLOW}
+                        onClick={() => goBackOrHomePage(this.props.history!)}>{this.props.intl.formatMessage({id: "close"})}</Button>,
+                this.getOutcomeBtns()]}
+        >
+          <Form onSubmit={this.validate} layout="vertical">
 
-                      <ReadonlyField
-                        entityName={PositionOverlappingRequest.NAME}
-                        propertyName="requestNumber"
-                        form={this.props.form}
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        disabled={true}
-                        getFieldDecoratorOpts={{
-                          rules: [{required: true,}]
-                        }}
-                      />
+            <ReadonlyField
+              entityName={PositionOverlappingRequest.NAME}
+              propertyName="requestNumber"
+              form={this.props.form}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              disabled={true}
+              getFieldDecoratorOpts={{
+                rules: [{required: true,}]
+              }}
+            />
 
-                      <ReadonlyField
-                        entityName={PositionOverlappingRequest.NAME}
-                        propertyName="status"
-                        disabled={true}
-                        form={this.props.form}
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        optionsContainer={this.statusesDc}
-                        getFieldDecoratorOpts={{
-                          rules: [{required: true,}],
-                        }}
-                      />
+            <ReadonlyField
+              entityName={PositionOverlappingRequest.NAME}
+              propertyName="status"
+              disabled={true}
+              form={this.props.form}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              optionsContainer={this.statusesDc}
+              getFieldDecoratorOpts={{
+                rules: [{required: true,}],
+              }}
+            />
 
-                      <ReadonlyField
-                        entityName={PositionOverlappingRequest.NAME}
-                        propertyName="requestDate"
-                        form={this.props.form}
-                        disabled={true}
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        format={DEFAULT_DATE_PATTERN}
-                        getFieldDecoratorOpts={{
-                          rules: [{required: true,}]
-                        }}
-                      />
+            <ReadonlyField
+              entityName={PositionOverlappingRequest.NAME}
+              propertyName="requestDate"
+              form={this.props.form}
+              disabled={true}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              format={DEFAULT_DATE_PATTERN}
+              getFieldDecoratorOpts={{
+                rules: [{required: true,}]
+              }}
+            />
 
-                      <Form.Item
-                        label={createElement(Msg, {entityName: entityName, propertyName: "personGroup"})}>
-                        <Input
-                          value={this.person ? this.person['_instanceName'] || '' : ''}
-                          disabled/>
-                      </Form.Item>
+            <Form.Item
+              label={createElement(Msg, {entityName: entityName, propertyName: "personGroup"})}>
+              <Input
+                value={this.person ? this.person['_instanceName'] || '' : ''}
+                disabled/>
+            </Form.Item>
 
-                      <ReadonlyField
-                        entityName={PositionOverlappingRequest.NAME}
-                        propertyName="type"
-                        form={this.props.form}
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        optionsContainer={this.positionOverlappingTypeDc}
-                        disabled={this.isNotDraft()!}
-                        getFieldDecoratorOpts={{
-                          rules: [{
-                            required: true,
-                            message: this.props.intl.formatMessage(
-                              {id: "form.validation.required"},
-                              {fieldName: messages[PositionOverlappingRequest.NAME + '.type']})
-                          }]
-                        }}
-                      />
+            <ReadonlyField
+              entityName={PositionOverlappingRequest.NAME}
+              propertyName="type"
+              form={this.props.form}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              optionsContainer={this.positionOverlappingTypeDc}
+              disabled={this.isNotDraft()!}
+              getFieldDecoratorOpts={{
+                rules: [{
+                  required: true,
+                  message: this.props.intl.formatMessage(
+                    {id: "form.validation.required"},
+                    {fieldName: messages[PositionOverlappingRequest.NAME + '.type']})
+                }]
+              }}
+            />
 
-                      <ReadonlyField
-                        entityName={PositionOverlappingRequest.NAME}
-                        propertyName="department"
-                        form={this.props.form}
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        optionsContainer={this.organizationGroupDc}
-                        disabled={true}
-                        getFieldDecoratorOpts={{
-                          rules: [{
-                            required: true,
-                            message: this.props.intl.formatMessage(
-                              {id: "form.validation.required"},
-                              {fieldName: messages[PositionOverlappingRequest.NAME + '.department']})
-                          }]
-                        }}
-                      />
+            <ReadonlyField
+              entityName={PositionOverlappingRequest.NAME}
+              propertyName="department"
+              form={this.props.form}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              optionsContainer={this.organizationGroupDc}
+              getFieldDecoratorOpts={{
+                rules: [{
+                  required: true,
+                  message: this.props.intl.formatMessage(
+                    {id: "form.validation.required"},
+                    {fieldName: messages[PositionOverlappingRequest.NAME + '.department']})
+                }],
+                getValueFromEvent: organizationId => {
+                  this.currentOrganization = organizationId
+                  return organizationId
+                }
+              }}
+            />
 
-                      <ReadonlyField
-                        entityName={PositionOverlappingRequest.NAME}
-                        propertyName="positionGroup"
-                        form={this.props.form}
-                        disabled={this.isNotDraft()!}
+            <ReadonlyField
+              entityName={PositionOverlappingRequest.NAME}
+              propertyName="positionGroup"
+              form={this.props.form}
+              disabled={this.isNotDraft()!}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              optionsContainer={this.positionGroupDc}
+              getFieldDecoratorOpts={{
+                rules: [{
+                  required: true,
+                  message: this.props.intl.formatMessage(
+                    {id: "form.validation.required"},
+                    {fieldName: messages[PositionOverlappingRequest.NAME + '.positionGroup']})
+                }],
+                getValueFromEvent: positionId => {
+                  this.fillOrganization(positionId);
+                  return positionId;
+                }
+              }}
+            />
 
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        optionsContainer={this.positionGroupDc}
-                        getFieldDecoratorOpts={{
-                          rules: [{
-                            required: true,
-                            message: this.props.intl.formatMessage(
-                              {id: "form.validation.required"},
-                              {fieldName: messages[PositionOverlappingRequest.NAME + '.positionGroup']})
-                          }],
-                          getValueFromEvent: positionId => {
-                            this.fillOrganization(positionId);
-                            return positionId;
-                          }
-                        }}
-                      />
 
-
-                      <Form onSubmit={this.validate} layout="vertical">
-                        <ReadonlyField
-                          entityName={PositionOverlappingRequest.NAME}
-                          propertyName="workCompletionDate"
-                          form={this.props.form}
-                          formItemOpts={{style: {marginBottom: "12px"}}}
-                          disabled={this.isNotDraft()!}
-                          getFieldDecoratorOpts={{
-                            rules: [{required: true,}]
-                          }}
-                        />
-                      </Form>
-                      <Form.Item
-                        label={createElement(Msg, {entityName: this.dataInstance.entityName, propertyName: "justification"})}>
-                        {this.props.form.getFieldDecorator("justification")(
-                          <TextArea
-                            disabled={this.isNotDraft()!}
-                            rows={4}/>
-                        )}
-                      </Form.Item>
-                      <ReadonlyField
-                        entityName={entityName}
-                        propertyName="attachment"
-                        form={this.props.form}
-                        disabled={this.isNotDraft()!}
-                        formItemOpts={{style: {marginBottom: "12px"}}}
-                        getFieldDecoratorOpts={{
-                          rules: [{
-                            required: true,
-                            message: this.props.intl.formatMessage({id: "form.validation.required"}, {fieldName: messages[entityName + '.attachment']})
-                          }]
-                        }}
-                      />
-                    </Form>
-                    {this.takCard()}
-                  </Card>
-              </Section>
-            </Page>;
+            <Form onSubmit={this.validate} layout="vertical">
+              <ReadonlyField
+                entityName={PositionOverlappingRequest.NAME}
+                propertyName="workCompletionDate"
+                form={this.props.form}
+                formItemOpts={{style: {marginBottom: "12px"}}}
+                disabled={this.isNotDraft()!}
+                getFieldDecoratorOpts={{
+                  rules: [{required: true,}]
+                }}
+              />
+            </Form>
+            <Form.Item
+              label={createElement(Msg, {entityName: this.dataInstance.entityName, propertyName: "justification"})}>
+              {this.props.form.getFieldDecorator("justification")(
+                <TextArea
+                  disabled={this.isNotDraft()!}
+                  rows={4}/>
+              )}
+            </Form.Item>
+            <ReadonlyField
+              entityName={entityName}
+              propertyName="attachment"
+              form={this.props.form}
+              disabled={this.isNotDraft()!}
+              formItemOpts={{style: {marginBottom: "12px"}}}
+              getFieldDecoratorOpts={{
+                rules: [{
+                  required: true,
+                  message: this.props.intl.formatMessage({id: "form.validation.required"}, {fieldName: messages[entityName + '.attachment']})
+                }]
+              }}
+            />
+          </Form>
+          {this.takCard()}
+        </Card>
+      </Section>
+    </Page>;
 
   }
 
-  fillOrganization = (positionId: string ) => {
+  fillOrganization = (positionId: string) => {
     restQueries.organizationGroupByPositionId(positionId).then(values => {
-        const organId = values.length<1? "" : values[0].id;
-        this.props.form.setFields({
-          department: {
-            value: organId
-          }
-        })
+      const organId = values.length < 1 ? "" : values[0].id;
+      this.props.form.setFields({
+        department: {
+          value: organId
+        }
+      })
     });
   }
 
   onReactionDisposerEffect = (item: PositionOverlappingRequest | undefined) => {
-
+    const organizationId = this.props.form.getFieldValue("department");
+    if (organizationId) {
+      this.currentOrganization = organizationId
+    }
     const personGroupId = item && item.personGroup ? item.personGroup.id! : this.props!.personGroupId!;
     const requestDate = item && item.requestDate ? item.requestDate : moment().toISOString();
     getCubaREST()!.searchEntities<PersonExt>(PersonExt.NAME, {
@@ -331,7 +349,7 @@ export default injectIntl(
     onValuesChange: (props: any, changedValues: any) => {
       // Reset server-side errors when field is edited
       Object.keys(changedValues).forEach((fieldName: string) => {
-      props.form.setFields({
+        props.form.setFields({
           [fieldName]: {
             value: changedValues[fieldName]
           }
